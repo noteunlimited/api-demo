caacb2f8054716d472d632152781da13ad03f1d5
Dima-Yakovlev <70764624+Dima-Yakovlev@users.noreply.github.com>
2022-07-01 15:01:26 +0300

    clear data

--- backend/host.py
+++ backend/host.py
@@ -1,4 +1,4 @@
 API_URL = "https://api-stage.noteunlimited.com"
-USER_API_EMAIL = "dima@noteunlimited.com"
-USER_API_KEY_NAME = "stage_key"
-USER_API_KEY = "NDQ4Nzg1NDU.0d94784ad7ad326c57c118623fca2ffec1b199ba81380a4da9596a159ccc87ac"
+USER_API_EMAIL = user_email
+USER_API_KEY_NAME = key_name
+USER_API_KEY = ***
